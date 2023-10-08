54403c40c38397721f4e083a574fc63dd8c99ed9
Valentino <malassisivalen@gmail.com>
2023-10-07 21:05:28 -0300

    modifique templates y agrege config

--- aplicacion/models/productos.model.php
+++ aplicacion/models/productos.model.php
@@ -1,9 +1,12 @@
 <?php
+
+require_once 'aplicacion/models/config.php';
+
 class ProductosModel {
     private $db;
 
     function __construct() {
-        $this->db = new PDO('mysql:host=localhost;dbname=db_tpe_web2;charset=utf8', 'root', '');
+        $this->db = new PDO('mysql:host='.DB_HOST . ';dbname='. DB_NAME .';charset=' . DB_Charset .',' . DB_USER . ','. DB_PASS);
     }
 
     function getProductos() {
