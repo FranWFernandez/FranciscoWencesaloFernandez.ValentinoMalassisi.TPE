f1215d5f16149b74d0f0285c7eb088f65e6fe60a
Valentino <malassisivalen@gmail.com>
2023-10-11 17:59:56 -0300

    add

--- aplicacion/models/productos.model.php
+++ aplicacion/models/productos.model.php
@@ -5,17 +5,12 @@ require_once './aplicacion/models/model.php';
 class ProductosModel extends DB {
 
     function getProductos() {
-        //$query = $this->connect()->prepare('SELECT FROM productos WHERE Producto =botines');
         $query = $this->connect()->prepare('SELECT productos.*, marcas.marca FROM productos 
                                     INNER JOIN marcas ON productos.id_marca = marcas.id_marca');
         //$query = $this->connect()->prepare('SELECT productos.*, categorias.categoria FROM productos 
                                     //INNER JOIN categorias ON productos.id_categoria = categorias.id_categoria');
         
 
-        /*SELECT CategoryName, ProductName
-            FROM Categories INNER JOIN Products
-            ON Categories.CategoryID = Products.CategoryID;
-            */
         $query->execute();
         
         $productos = $query->fetchAll(PDO::FETCH_OBJ);
