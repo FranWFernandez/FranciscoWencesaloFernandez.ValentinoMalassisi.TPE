2f7d68ef935c2dcc911ce6d7a57cbb82d43f7f61
FranWFernandez <145360524+FranWFernandez@users.noreply.github.com>
2023-10-12 14:22:46 -0300

    relaizo cambios

--- aplicacion/controllers/productos.controller.php
+++ aplicacion/controllers/productos.controller.php
@@ -1,16 +1,22 @@
 <?php
 require_once 'aplicacion/models/productos.model.php';
+require_once 'aplicacion/models/categorias.model.php';
+require_once 'aplicacion/models/marcas.model.php';
 require_once 'aplicacion/views/productos.view.php';
 require_once 'aplicacion/helpers/autenticar.helper.php';
 
 class ProductosController {
     private $model;
+    private $modelMarca;
+    private $modelCategoria;
     private $view;
     
     public function __construct() {
         AutenticarHelper::verify();
 
         $this->model = new ProductosModel();
+        $this->modelMarca = new MarcasModel();
+        $this->modelCategoria = new CategoriasModel();
         $this->view = new ProductosView();
     }
 
@@ -18,6 +24,16 @@ class ProductosController {
         $Productos = $this->model->getProductos();
         $this->view->showProductos($Productos);
     }
+
+    /*public function showMarcas() {
+        $marcas = $this->modelMarca->getMarcas();
+        $this->view->showProductos($marcas);
+    }
+
+    public function showCategorias() {
+        $categorias = $this->modelCategoria->getCategorias();
+        $this->view->showCategorias($categorias);
+    }*/
 
     public function addProducto() {
 
@@ -35,7 +51,7 @@ class ProductosController {
 
         $id = $this->model->insertProducto($producto, $precio, $talle, $id_categoria, $id_marca);
         if ($id) {
-            header('Location: ' . BASE_URL);
+            header('Location: ' . BASE_URL . '/listar');
         } else {
             $this->view->showError("Error al insertar el producto");
         }
@@ -43,6 +59,6 @@ class ProductosController {
 
     function removeProducto($id) {
         $this->model->deleteProducto($id);
-        header('Location: ' . BASE_URL);
+        header('Location: ' . BASE_URL . '/listar');
     }
 }
